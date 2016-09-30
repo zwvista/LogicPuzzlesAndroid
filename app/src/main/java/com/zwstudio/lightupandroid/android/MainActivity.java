44273b621c278ee46aed5e2ab1ae02f5d8de14f3
趙偉 <zwvista@msn.com>
2016-09-30 09:41:29 +0900

    Position

--- app/src/main/java/com/zwstudio/lightupandroid/android/MainActivity.java
+++ app/src/main/java/com/zwstudio/lightupandroid/android/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.zwstudio.lightupandroid.R;
+import com.zwstudio.lightupandroid.data.GameDocument;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GameDocument doc = new GameDocument();
+        InputStream in_s = null;
+        try {
+            in_s = getApplicationContext().getAssets().open("Levels.xml");
+            doc.loadXml(in_s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/zwstudio/lightupandroid/data/GameDocument.java
+++ app/src/main/java/com/zwstudio/lightupandroid/data/GameDocument.java
@@ -1,8 +1,69 @@
 package com.zwstudio.lightupandroid.data;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Created by zwvista on 2016/09/29.
  */
 
 public class GameDocument {
+    public Map<String, List<String>> levels = new HashMap<>();
+
+    public void loadXml(InputStream in_s) {
+        XmlPullParserFactory pullParserFactory;
+        try {
+            pullParserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = pullParserFactory.newPullParser();
+
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in_s, null);
+
+            parseXML(parser);
+
+        } catch (XmlPullParserException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void parseXML(XmlPullParser parser) throws XmlPullParserException,IOException
+    {
+        int eventType = parser.getEventType();
+
+        while (eventType != XmlPullParser.END_DOCUMENT){
+            String name = null;
+            String id = null;
+            List<String> layout = null;
+            switch (eventType){
+                case XmlPullParser.START_DOCUMENT:
+                    break;
+                case XmlPullParser.START_TAG:
+                    name = parser.getName();
+                    if (name.equals("level")){
+                        id = parser.getAttributeValue(null,"id");
+                        layout = Arrays.asList(parser.nextText().split("\n"));
+                        layout = layout.subList(2, layout.size() - 2)
+                                .stream().map(s -> s.substring(0, s.length() - 1))
+                                .collect(Collectors.toList());
+                        levels.put(id, layout);
+                    }
+                    break;
+                case XmlPullParser.END_TAG:
+                    break;
+            }
+            eventType = parser.next();
+        }
+
+    }
 }
