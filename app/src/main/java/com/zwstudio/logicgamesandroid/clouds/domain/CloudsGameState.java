cee0e3143182c4586082128fbbea10ae08ac76e7
zwvista <zwvista@msn.com>
2016-10-26 17:31:24 +0900

    clouds 5

--- app/src/main/java/com/zwstudio/logicgamesandroid/clouds/domain/CloudsGameState.java
+++ app/src/main/java/com/zwstudio/logicgamesandroid/clouds/domain/CloudsGameState.java
@@ -1,11 +1,19 @@
 package com.zwstudio.logicgamesandroid.clouds.domain;
 
+import com.zwstudio.logicgamesandroid.logicgames.domain.Graph;
 import com.zwstudio.logicgamesandroid.logicgames.domain.LogicGamesHintState;
+import com.zwstudio.logicgamesandroid.logicgames.domain.Node;
 import com.zwstudio.logicgamesandroid.logicgames.domain.Position;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import fj.F;
+
+import static fj.data.HashMap.fromMap;
+import static fj.data.List.iterableList;
 
 /**
  * Created by zwvista on 2016/09/29.
@@ -64,6 +72,40 @@ public class CloudsGameState {
             if (n1 != n2) isSolved = false;
         }
         if (!isSolved) return;
+        Graph g = new Graph();
+        Map<Position, Node> pos2node = new HashMap<>();
+        for (int r = 0; r < rows(); r++)
+            for (int c = 0; c < cols(); c++) {
+                Position p = new Position(r, c);
+                if (get(p) != CloudsObject.Cloud) continue;
+                Node node = new Node(p.toString());
+                g.addNode(node);
+                pos2node.put(p, node);
+            }
+        for (Position p : pos2node.keySet())
+            for (Position os : CloudsGame.offset) {
+                Position p2 = p.add(os);
+                if (pos2node.containsKey(p2))
+                    g.connectNode(pos2node.get(p), pos2node.get(p2));
+            }
+        while (!pos2node.isEmpty()) {
+            g.setRootNode(iterableList(pos2node.values()).head());
+            List<Node> nodeList = g.bfs();
+            int r2 = 0, r1 = rows(), c2 = 0, c1 = cols();
+            for (Node node : nodeList) {
+                Position p = fromMap(pos2node).toStream().find(e -> e._2().equals(node)).some()._1();
+                pos2node.remove(p);
+                if (r2 < p.row) r2 = p.row;
+                if (r1 > p.row) r1 = p.row;
+                if (c2 < p.col) c2 = p.col;
+                if (c1 > p.col) c1 = p.col;
+            }
+            int rs = r2 - r1 + 1, cs = c2 - c1 + 1;
+            if (!(rs >= 2 && cs >= 2 && rs * cs == nodeList.size())) {
+                isSolved = false;
+                return;
+            }
+        }
     }
 
     public boolean setObject(CloudsGameMove move) {
