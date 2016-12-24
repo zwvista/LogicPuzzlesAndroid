fb58a54d84209caea87866a5810fb3301641cc93
zwvista <zwvista@msn.com>
2016-12-24 15:18:24 +0900

    Tents 2

--- app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/tents/domain/TentsTentObject.java
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/tents/domain/TentsTentObject.java
@@ -9,6 +9,6 @@ import com.zwstudio.logicpuzzlesandroid.common.domain.AllowedObjectState;
 public class TentsTentObject extends TentsObject {
     public AllowedObjectState state = AllowedObjectState.Normal;
     public String objAsString() {
-        return "tree";
+        return "tent";
     }
 }
