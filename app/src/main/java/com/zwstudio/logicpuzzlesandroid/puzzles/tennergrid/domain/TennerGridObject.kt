21fcd82df3e2137655cda620d68c6458a7c86a06
趙偉 <zwvista@msn.com>
2020-04-12 14:15:46 +0900

    to Kotlin 10

---
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/tennergrid/domain/TennerGridObject.kt
@@ -0,0 +1,5 @@
+package com.zwstudio.logicpuzzlesandroid.puzzles.tennergrid.domainimport
+
+import com.zwstudio.logicpuzzlesandroid.common.domain.Position
+
+class TennerGridGameMove(val p: Position, var obj: Int = 0)
---
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/theoddbrick/domain/TheOddBrickObject.kt
@@ -0,0 +1,5 @@
+package com.zwstudio.logicpuzzlesandroid.puzzles.theoddbrick.domainimport
+
+import com.zwstudio.logicpuzzlesandroid.common.domain.Position
+
+class TheOddBrickGameMove(val p: Position, var obj: Int = 0)
