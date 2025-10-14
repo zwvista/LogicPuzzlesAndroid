f4edbb0f1eaec18424d863233fb601eb13f0b302
zwvista <zwvista@msn.com>
2025-10-14 17:30:43 +0900

    intValue1

--- app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/hidoku/HidokuDocument.kt
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/hidoku/HidokuDocument.kt
@@ -9,8 +9,9 @@ class HidokuDocument(context: Context) : GameDocument<HidokuGameMove>(context) {
     override fun saveMove(move: HidokuGameMove, rec: MoveProgress) {
         rec.row = move.p.row
         rec.col = move.p.col
+        rec.intValue1 = move.obj
     }
 
     override fun loadMove(rec: MoveProgress) =
-        HidokuGameMove(Position(rec.row, rec.col))
+        HidokuGameMove(Position(rec.row, rec.col), rec.intValue1)
 }
