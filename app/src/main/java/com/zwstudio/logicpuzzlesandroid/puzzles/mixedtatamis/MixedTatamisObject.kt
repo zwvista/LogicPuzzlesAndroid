f1b73ef3c948363869b3ac69744af454e93f6d91
趙偉 <zwvista@msn.com>
2026-01-04 11:41:32 +0900

    MixedTatamis 1

---
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/mixedtatamis/MixedTatamisObject.kt
@@ -0,0 +1,6 @@
+package com.zwstudio.logicpuzzlesandroid.puzzles.mixedtatamis
+
+import com.zwstudio.logicpuzzlesandroid.common.domain.GridLineObject
+import com.zwstudio.logicpuzzlesandroid.common.domain.Position
+
+class MixedTatamisGameMove(val p: Position, var dir: Int = 0, var obj: GridLineObject = GridLineObject.Empty)
--- rename_and_replace.sh
+++ rename_and_replace.sh
@@ -3,10 +3,10 @@ set -e
 
 # 协程变量（源字符串和目标字符串）
 SRC="BoxItAgain"
-DST="WallHints"
+DST="MixedTatamis"
 
 # 切换到 Branches 目录
-cd "$(dirname "$0")/app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/wallhints"
+cd "$(dirname "$0")/app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/mixedtatamis"
 
 # 1. 改文件名
 for f in ${SRC}*; do
