1cd7297cef0947e55e78c9a1d979b87a6c41cc8e
趙偉 <zwvista@msn.com>
2026-03-01 05:10:22 +0900

    Tents 8

--- app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/tents/TentsGameState.kt
+++ app/src/main/java/com/zwstudio/logicpuzzlesandroid/puzzles/tents/TentsGameState.kt
@@ -44,11 +44,10 @@ class TentsGameState(game: TentsGame) : CellsGameState<TentsGame, TentsGameMove,
     }
 
     /*
-        iOS Game: Logic Games/Puzzle Set 1/Tents
+        iOS Game: 100 Logic Games/Puzzle Set 1/Tents
 
         Summary
-        Each camper wants to put his Tent under the shade of a Tree. But he also
-        wants his privacy!
+        Each camper wants his shade. But his privacy too!
 
         Description
         1. The board represents a camping field with many Trees. Campers want to set
@@ -58,8 +57,9 @@ class TentsGameState(game: TentsGame) : CellsGameState<TentsGame, TentsGameMove,
            Tents near them, not even diagonally.
         3. The numbers on the borders tell you how many Tents there are in that row
            or column.
-        4. Finally, each Tree has at least one Tent touching it, horizontally or
-           vertically.
+        4. Finally, keep in mind these two rules:
+        5. Each Tree has at least one Tent touching it, horizontally or vertically.
+        6. There's the same exact numbers of Trees and Tents in the Camping Area.
     */
     private fun updateIsSolved() {
         val allowedObjectsOnly = game.gdi.isAllowedObjectsOnly
@@ -90,10 +90,10 @@ class TentsGameState(game: TentsGame) : CellsGameState<TentsGame, TentsGameMove,
             for (c in 0 until cols)
                 if (this[r, c] is TentsForbiddenObject)
                     this[r, c] = TentsEmptyObject
+        var (nTree, nTent) = 0 to 0
         for (r in 0 until rows)
             for (c in 0 until cols) {
                 val p = Position(r, c)
-                val o = this[r, c]
                 fun hasTree(): Boolean {
                     for (os in TentsGame.offset) {
                         val p2 = p + os
@@ -110,26 +110,38 @@ class TentsGameState(game: TentsGame) : CellsGameState<TentsGame, TentsGameMove,
                     }
                     return false
                 }
-                if (o is TentsTentObject) {
-                    // 1. The board represents a camping field with many Trees. Campers want to set
-                    // their Tent in the shade, horizontally or vertically adjacent to a Tree(not
-                    // diagonally).
-                    // 2. At the same time they need their privacy, so a Tent can't have any other
-                    // Tents near them, not even diagonally.
-                    val s = if (hasTree() && !hasTent(false)) AllowedObjectState.Normal else AllowedObjectState.Error
-                    o.state = s
-                    if (s == AllowedObjectState.Error) isSolved = false
-                } else if (o is TentsTreeObject) {
-                    // 4. Finally, each Tree has at least one Tent touching it, horizontally or
-                    // vertically.
-                    val s = if (hasTent(true)) AllowedObjectState.Normal else AllowedObjectState.Error
-                    o.state = s
-                    if (s == AllowedObjectState.Error) isSolved = false
-                } else if ((o is TentsEmptyObject || o is TentsMarkerObject) && allowedObjectsOnly &&
-                        (col2state[c] != HintState.Normal && game.col2hint[c] != TentsGame.PUZ_UNKNOWN ||
-                        row2state[r] != HintState.Normal && game.row2hint[r] != TentsGame.PUZ_UNKNOWN ||
-                        !hasTree() || hasTent(false)))
-                    this[r, c] = TentsForbiddenObject
+                when (val o = this[r, c]) {
+                    is TentsTentObject -> {
+                        nTent++
+                        // 1. The board represents a camping field with many Trees. Campers want to set
+                        // their Tent in the shade, horizontally or vertically adjacent to a Tree(not
+                        // diagonally).
+                        // 2. At the same time they need their privacy, so a Tent can't have any other
+                        // Tents near them, not even diagonally.
+                        val s =
+                            if (hasTree() && !hasTent(false)) AllowedObjectState.Normal else AllowedObjectState.Error
+                        o.state = s
+                        if (s == AllowedObjectState.Error) isSolved = false
+                    }
+                    is TentsTreeObject -> {
+                        nTree++
+                        // 5. Each Tree has at least one Tent touching it, horizontally or vertically.
+                        val s =
+                            if (hasTent(true)) AllowedObjectState.Normal else AllowedObjectState.Error
+                        o.state = s
+                        if (s == AllowedObjectState.Error) isSolved = false
+                    }
+                    is TentsEmptyObject, is TentsMarkerObject ->
+                        if (allowedObjectsOnly &&
+                            (col2state[c] != HintState.Normal && game.col2hint[c] != TentsGame.PUZ_UNKNOWN ||
+                            row2state[r] != HintState.Normal && game.row2hint[r] != TentsGame.PUZ_UNKNOWN ||
+                            !hasTree() || hasTent(false))
+                        )
+                            this[r, c] = TentsForbiddenObject
+                    else -> {}
+                }
             }
+        // 6. There's the same exact numbers of Trees and Tents in the Camping Area.
+        if (nTree != nTent) isSolved = false
     }
 }
